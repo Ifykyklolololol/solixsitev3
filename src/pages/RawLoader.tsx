feadec782a31cf2ea02a3fc93b94ac718db1500e
Ifykyklolololol <solixhub@gmail.com>
2025-08-30 03:55:37 +0200

    Update RawLoader.tsx

--- src/pages/RawLoader.tsx
+++ src/pages/RawLoader.tsx
@@ -1,9 +1,1 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const lua = `loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/solixloader/refs/heads/main/solix%20v2%20new%20loader.lua"))()`;
-
-  return new NextResponse(lua, {
-    headers: { "Content-Type": "text/plain" }
-  });
-}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/solixloader/refs/heads/main/solix%20v2%20new%20loader.lua"))()
